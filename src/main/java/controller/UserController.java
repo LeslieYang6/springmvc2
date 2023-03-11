c479ecc7fee035f8fed0fa83f1cae09cdda54349
LeslieYang6 <leslieyang320@gmail.com>
2023-03-11 23:04:49 +0100

    save code

--- src/main/java/controller/UserController.java
+++ src/main/java/controller/UserController.java
@@ -7,12 +7,19 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
-
+@RequestMapping("/user")
 public class UserController {
   @RequestMapping("/save")
   @ResponseBody
   public String save(){
     System.out.println("User save:");
-    return "{'info':'firstSpringmvc'}";
+    return "{'info':'user save'}";
+  }
+
+  @RequestMapping("/commonParam")
+  @ResponseBody
+  public String CommonParam()
+  {
+    return "{'module':'common param'}";
   }
 }
