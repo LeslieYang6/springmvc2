c2025acf0e84f38b8d30cce8d14f861b26035372
LeslieYang6 <leslieyang320@gmail.com>
2023-03-12 22:40:17 +0100

    save code

--- src/main/java/config/servletContainersInitConfig.java
+++ src/main/java/config/servletContainersInitConfig.java
@@ -2,22 +2,34 @@ package config;
 
 import org.springframework.web.context.WebApplicationContext;
 import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
+import org.springframework.web.filter.CharacterEncodingFilter;
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
 import org.springframework.web.servlet.support.AbstractDispatcherServletInitializer;
 
+import javax.servlet.Filter;
 import javax.swing.*;
 
-public class servletContainersInitConfig extends AbstractDispatcherServletInitializer {
-  protected WebApplicationContext createServletApplicationContext(){
-    AnnotationConfigWebApplicationContext ctx=new AnnotationConfigWebApplicationContext();
-    ctx.register(SpringMVCConfig.class);
-    return ctx;
+public class servletContainersInitConfig extends AbstractAnnotationConfigDispatcherServletInitializer {
+
+  @Override
+  protected Class<?>[] getRootConfigClasses() {
+    return new Class[0];
   }
 
-  protected  String[] getServletMappings(){
+  @Override
+  protected Class<?>[] getServletConfigClasses() {
+    return new Class[]{ SpringMVCConfig.class};
+  }
+
+  @Override
+  protected String[] getServletMappings() {
     return new String[]{"/"};
   }
-  protected  WebApplicationContext createRootApplicationContext()
-  {
-    return null;
+
+  @Override
+  protected Filter[] getServletFilters() {
+    CharacterEncodingFilter filter=new CharacterEncodingFilter();
+    filter.setEncoding("UTF-8");
+    return new Filter[]{filter};
   }
 }
--- src/main/java/controller/UserController.java
+++ src/main/java/controller/UserController.java
@@ -1,10 +1,15 @@
 package controller;
 
+import domain.User;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.Arrays;
+import java.util.List;
 
 @Controller
 @RequestMapping("/user")
@@ -18,8 +23,61 @@ public class UserController {
 
   @RequestMapping("/commonParam")
   @ResponseBody
-  public String CommonParam()
+  public String CommonParam(String name, int age)
   {
+    System.out.println("普通参数传递 name:"+name);
+    System.out.println("普通参数传递 age:"+age);
     return "{'module':'common param'}";
   }
+
+  @RequestMapping("/commonParamDifferentName")
+  @ResponseBody
+  public String CommonParamDifferentName(@RequestParam("name") String userName, int age)
+  {
+    System.out.println("普通参数传递 name:"+userName);
+    System.out.println("普通参数传递 age:"+age);
+    return "{'module':'common param different name'}";
+  }
+/*
+  @RequestMapping("/pojoParam")
+  @ResponseBody
+  public String pojoParam(User user)
+  {
+    System.out.println("pojo参数传递 user:"+user);
+    return "{'module':'pojo param'}";
+  }
+*/
+  @RequestMapping("/pojoContainPojoParam")
+  @ResponseBody
+  public String pojoContainPojoParam(User user)
+  {
+    System.out.println("pojo参数传递 user:"+user);
+    return "{'module':'pojoContainPojoParam'}";
+  }
+
+  @RequestMapping("/arrayParam")
+  @ResponseBody
+  public String arrayParam(String[] likes)
+  {
+    System.out.println("pojo参数传递 user:"+ Arrays.toString(likes));
+    return "{'module':'arrayParam'}";
+  }
+
+  @RequestMapping("/listParam")
+  @ResponseBody
+  public String arrayParam(@RequestParam List<String> likes)
+  {
+    System.out.println("pojo参数传递 user:"+ likes);
+    return "{'module':'arrayParam'}";
+  }
+
+  @RequestMapping("/paramforJson")
+  @ResponseBody
+  public String paramforJson(User user)
+  {
+    System.out.println("pojo参数传递 user:"+ user);
+    return "{'module':'arrayParam'}";
+  }
+
+
 }
